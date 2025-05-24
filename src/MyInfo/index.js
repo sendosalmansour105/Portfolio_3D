f2b105d01caaa0c6783bf6114f7d578fb77cf5ca
Sendos Almansour <seses0319@gmail.com>
2025-05-24 03:01:41 +0300

    Update index.js

--- src/MyInfo/index.js
+++ src/MyInfo/index.js
@@ -150,7 +150,7 @@ export const experiences = [
   {
     title: "Darribni",
     job: "Front-End Developer",
-    date: "2021-2023",
+    date: "Aug 2023 - Jan 2024",
     contents: [
       "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
       "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
@@ -162,7 +162,7 @@ export const experiences = [
   {
     title: "TAD-Center",
     job: "Front-End Develope",
-    date: "2023-2024",
+    date: "Feb 2024 - May 2024",
     contents: [
       "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
       "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
@@ -172,9 +172,9 @@ export const experiences = [
     ],
   },
   {
-    title: "Freelance Developer",
-    job: "Self-Employed",
-    date: "2025-Present",
+    title: "Bradosti",
+    job: "Front-End Developer",
+    date: " Jul 2024 - Aug 2024",
     contents: [
       "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
       "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
