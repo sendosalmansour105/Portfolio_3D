99610868a91c597aa0604bfc121b9359d0eea21b
sendos105 <seses0319@gmail.com>
2025-05-24 03:08:52 +0300

    Updates

--- src/MyInfo/index.js
+++ src/MyInfo/index.js
@@ -153,9 +153,7 @@ export const experiences = [
     date: "Aug 2023 - Jan 2024",
     contents: [
       "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
-      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
-      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
-      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
+      "Designed and implemented intuitive map interfaces using MapsUI, enhancinging C++ and the ility of applications using Windows Forms and WPF frameworks.",
       "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
     ],
   },
@@ -165,10 +163,7 @@ export const experiences = [
     date: "Feb 2024 - May 2024",
     contents: [
       "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
-      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
-      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
-      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
-      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+      "Developed back-end systems enabling vehicle-to-cloud coupdates, real-time tracking, and remote start capabilities.",
     ],
   },
   {
@@ -177,7 +172,7 @@ export const experiences = [
     date: " Jul 2024 - Aug 2024",
     contents: [
       "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
-      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+      "Continuously enhacning technical skills and expanding expertise in modern web development and back-end technologies.",
     ],
   },
 ];
